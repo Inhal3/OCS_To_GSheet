2e18c1c9c0fdfd2d98fd2c3a9bb432195f9cc07d
Mark Arkhipychev <hackyourrself@gmail.com>
2023-03-15 18:14:04 +0300

    Little changes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from get_goods import get_items
 from gsheet_upload_client import sheet_upload
-import datetime
 
 if __name__ == '__main__':
-    print(datetime.datetime.now())
     get_items()
-    sheet_upload()
+    sheet_upload(

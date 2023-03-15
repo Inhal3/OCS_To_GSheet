1da2847851d9b9d3d1efe27be016884581cb5dee
Mark Arkhipychev <hackyourrself@gmail.com>
2023-03-15 18:11:55 +0300

    Added service list func

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from get_goods import get_items
-from gsheet_upload import sheet_upload
+from gsheet_upload_client import sheet_upload
+import datetime
 
 if __name__ == '__main__':
+    print(datetime.datetime.now())
     get_items()
     sheet_upload()
